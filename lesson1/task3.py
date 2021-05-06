103d4174f60a5a86fd5256565b07523648455af3
Artemy <artemiy.morozov.00@mail.ru>
2021-05-06 15:35:05 +0300

    task3

---
+++ lesson1/task3.py
@@ -0,0 +1,41 @@
+x1 = 4.33
+x2 = 'hi'
+x3 = [55.2, 66.2]
+x4 = {55.5:{('kek0',), ('kek',)}, 55.1:{('no kek0',), ('no kek',)}}
+x5 = ('qqq', 33.3)
+x6 = {33.3:2.2, 22.2:3.3}
+x7 = {'one', 'two', 'three'}
+x8 = True
+x9 = {True, False}
+x10 = {(55.7, 4, 'pp')}
+x11 = 999
+x12 = ['www', 'eeee']
+x13 = {(5.3, False, 'oo'):True, (5.3, False, 'oo'):False}
+x14 = {'ok':'da', 'no':'net'}
+x15 = (90,)
+x16 = {(33.33, 12.12):[True, False], (11.22, 44.44):[False, True]}
+x17 = [False, True, True]
+x18 = {1, 2.22, 'lox', False}
+x19 = [1, 2, 55]
+x20 = [1, 'lol', 2.99]
+x21 = {2:[444, 2.22], 3:['qq', 2]}
+x22 = (4, True)
+x23 = (2.11, 'str')
+x24 = (True,)
+x25 = ('rr', 'gg')
+x26 = (2, True, 9.99)
+x27 = {1:{'one', 'two'}, 22:{'ee', 'tt'}}
+x28 = (4.4, 5.5)
+x29 = {1, 2, 3, 4, 5}
+x30 = {(False, 2, 22):5.5, (True, 1, 11):2.2}
+x31 = {1.1:1, 2.2:2}
+x32 = (False, 6.66)
+x33 = [(False, 3.3), (True, 2.2)]
+x34 = [(1,), (5,)]
+x35 = (True, 'str')
+x36 = (3.3,)
+
+List = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24, x25, x26, x27, x28, x29, x30, x31, x32, x33, x34, x35, x36]
+
+for i in range(len(List)):
+    print(str(i+1) + '.  ' + str(List[i]))
