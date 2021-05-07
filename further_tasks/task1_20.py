3c092c194c0b8282a90d93d6b7f82552b7d38103
Artemy <artemiy.morozov.00@mail.ru>
2021-05-07 16:47:31 +0300

    07.05.21

--- further_tasks/task1_20.py
+++ further_tasks/task1_20.py
@@ -67,3 +67,101 @@ def f7(ls):
     return new_ls
 
 # print(f7([1234, 2421, 5550, 5055, 1905, 3222, 1247, 9999, 4124, 1059]))
+
+def f8(x):
+    count = 2
+    b = True
+    while count < x:
+        if x % count == 0:
+            b = False
+        count += 1
+    return b
+
+# print(f8(809))
+
+def f9(x):
+    count = 2
+    ls = []
+    while count < x:
+        if x % count == 0:
+            ls.append(count)
+        count += 1
+    return ls
+
+# print(f9(15))
+
+def f10(a, n):
+    power = a
+    while n > 1:
+        power *= a
+        n -= 1
+    return power
+
+# print(f10(2, 6))
+
+def f11(ls):
+    count = 0
+    for x in ls:
+        if max(ls) == x:
+            count += 1
+    return max(ls), count
+
+# print(f11([1, 2, 3, 4, 51, 101, 101, 100, 101, 2, 66]))
+
+import math
+def f12():
+    radius = int(input('Введите радиус круга: '))
+    quantity = int(input('Введите количество точек: '))
+    count = 0
+    ls = []
+    ls_answer = []
+    while count < quantity:
+        xy_dot = input('Введите координаты точки ' + str(count + 1) + ' x,y: ')
+        coo = xy_dot.split(',')
+        s = math.sqrt(int(coo[0])**2 + int(coo[1])**2)
+        if s > radius:
+            ls.append(s)
+            ls_answer.append((coo[0], coo[1]))
+        count += 1
+    count = 0
+    for x in ls:
+        if x == min(ls):
+            return ls_answer[count]
+        count += 1
+
+# print(f12())
+
+def f13():
+    x = int(input('Введите координату проверяемой точки: '))
+    quantity = int(input('Введите количество отрезков: '))
+    count = 0
+    count1 = 0
+    while count < quantity:
+        xy_dot = input('Введите координаты отрезка ' + str(count + 1) + ' xmin, xmax: ')
+        xmin, xmax = xy_dot.split(',')
+        if (x > int(xmin)) and (x < int(xmax)):
+            count1 += 1
+        count += 1
+    if count1 == quantity:
+        return True
+    else:
+        return False
+
+# print(f13())
+
+def f15():
+    quantity = 0
+    while quantity < 3:
+        quantity = int(input('Введите количество сторон(>3): '))
+    count = 0
+    ls = []
+    while count < quantity:
+        ls.append(int(input('Введите количество плит на стороне ' + str(count + 1) + ': ')))
+        count += 1
+    if sum(ls) % 2 == 0:
+        return int(sum(ls) / 2)
+    else:
+        return int(int(sum(ls) / 2) + 1)
+
+# print(f15())
+
